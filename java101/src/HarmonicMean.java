7597813f4ad1a04434ab9e7aa7161903a9e5607f
Enes Sevim <sevim.enes06@gmail.com>
2026-04-21 12:42:36 +0300

    HarmonicMean

---
+++ java101/src/HarmonicMean.java
@@ -0,0 +1,16 @@
+public class HarmonicMean {
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        double harmonicSum = 0.0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            harmonicSum += 1.0 / numbers[i];
+        }
+
+        double harmonicMean = numbers.length / harmonicSum;
+
+        System.out.println("Harmonic Mean: " + harmonicMean);
+    }
+}
